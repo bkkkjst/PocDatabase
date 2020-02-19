f277a51ccd2b83c0dcbb44ad60e779ee28ba87e8
Theerawat Phuna <theerawat@orcsoft.co.th>
2020-02-19 14:41:16 +0700

    first commit

--- src/main/java/com/example/demo/repo/CountryRepo.java
+++ src/main/java/com/example/demo/repo/CountryRepo.java
@@ -1,3 +1,4 @@
+package com.example.demo.repo;
 //package com.example.demo.dao;
 //
 //import org.springframework.data.repository.CrudRepository;
--- src/main/java/com/example/demo/repo/CountryRepository.java
+++ src/main/java/com/example/demo/repo/CountryRepository.java
@@ -1,4 +1,4 @@
-package com.example.demo.dao;
+package com.example.demo.repo;
 
 
 import org.springframework.data.repository.CrudRepository;
