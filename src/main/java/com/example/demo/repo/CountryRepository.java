d773acb7c12687bb16a73d731cf3edd601acfa53
Theerawat Phuna <theerawat@orcsoft.co.th>
2020-02-19 15:48:56 +0700

    include query

--- src/main/java/com/example/demo/repo/CountryRepository.java
+++ src/main/java/com/example/demo/repo/CountryRepository.java
@@ -1,10 +1,16 @@
 package com.example.demo.repo;
 
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 import com.example.demo.model.Country;
 
 
 public interface CountryRepository extends CrudRepository<Country, String>{
+	
+	List<Country> findByCountryName(@Param("name") String name);
 
 }
