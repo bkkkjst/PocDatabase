f98285095898ac3c0f3190de9719fcba7bbabd6c
Theerawat Phuna <theerawat@orcsoft.co.th>
2020-02-20 16:14:25 +0700

    update query

--- src/main/java/com/example/demo/repo/JobHistoryRepository.java
+++ src/main/java/com/example/demo/repo/JobHistoryRepository.java
@@ -1,10 +1,20 @@
 package com.example.demo.repo;
 
-import org.springframework.data.repository.CrudRepository;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.example.demo.model.Job;
 import com.example.demo.model.JobHistory;
 import com.example.demo.model.JobHistoryPK;
 
 public interface JobHistoryRepository extends CrudRepository<JobHistory, JobHistoryPK> {
-
+	
+	List<JobHistory> findByEndDateAfter(@Param("date") Date value);
+	
+	List<JobHistory> findByEndDateBefore(@Param("date") Date value);
+	
 }
--- src/main/java/com/example/demo/repo/JobRepository.java
+++ src/main/java/com/example/demo/repo/JobRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repo;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
@@ -9,6 +10,50 @@ import com.example.demo.model.Job;
 
 public interface JobRepository extends CrudRepository<Job, String>{
 	
-	List<Job> findByJobTitle(@Param("title") String name);
+	List<Job> findAllByJobTitleNotNull();
+	
+	List<Job> readAllByJobTitleNotNull();
+	
+	List<Job> findByJobTitleIgnoreCaseContaining(@Param("title") String name);
+	
+	int countByJobTitleNotNull();
+	
+	int countByJobTitleIgnoreCaseContaining(@Param("title") String name);
+	
+	List<Job> findOneByJobTitle(@Param("title") String name);
+	
+	boolean existsByJobTitleIgnoreCaseContaining(@Param("title") String name);
+	
+	//Test Support keyword
+	
+	List<Job> findByJobIdAndJobTitle(@Param("id") String id, @Param("title") String title);
+	
+	List<Job> findByJobIdOrJobTitle(@Param("id") String id, @Param("title") String title);
+	
+	List<Job> findByJobTitleIs(@Param("title") String title);
+	
+	List<Job> findByJobTitleEquals(@Param("title") String title);
+	
+	List<Job> findByMaxSalaryBetween(@Param("min") BigDecimal min, @Param("max") BigDecimal max);
+	
+	List<Job> findByMaxSalaryLessThan(@Param("value") BigDecimal value);
+	
+	List<Job> findByMaxSalaryLessThanEqual(@Param("value") BigDecimal value);
+	
+	List<Job> findByMaxSalaryGreaterThan(@Param("value") BigDecimal value);
+	
+	List<Job> findByMaxSalaryGreaterThanEqual(@Param("value") BigDecimal value);
+	
+	List<Job> findByJobTitleLike(@Param("title") String title);
+	
+	List<Job> findByJobTitleNotLike(@Param("title") String title);
+	
+	List<Job> findByJobTitleStartingWith(@Param("title") String title);
+	
+	List<Job> findByJobTitleEndingWith(@Param("title") String title);
+	
+	List<Job> findByJobTitleOrderByJobTitleDesc(@Param("title") String title);
+	
+	List<Job> findByJobTitleIgnoreCaseContainingOrderByJobTitleDesc(@Param("title") String title);
 
 }
