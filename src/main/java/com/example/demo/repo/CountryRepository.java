e78987796298ec8d7e885db8db5c143767da5308
bkkkjst <bkk_kjst02279@hotmail.com>
2020-02-24 10:26:48 +0700

    re-commit

--- src/main/java/com/example/demo/repo/CountryRepository.java
+++ src/main/java/com/example/demo/repo/CountryRepository.java
@@ -1,10 +1,17 @@
 package com.example.demo.repo;
 
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 import com.example.demo.model.Country;
 
 
 public interface CountryRepository extends CrudRepository<Country, String>{
+
 
 }
--- src/main/java/com/example/demo/repo/DepartmentRepository.java
+++ src/main/java/com/example/demo/repo/DepartmentRepository.java
@@ -1,9 +1,18 @@
 package com.example.demo.repo;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.model.Department;
+import com.example.demo.model.Employee;
 
-public interface DepartmentRepository extends CrudRepository<Department, Long>{
+public interface DepartmentRepository extends CrudRepository<Department, Long> {
+	
+	// @Query("from Department d where d.departmentName = :departmentName")
+	public Collection<Department> findByDepartmentNameIgnoreCaseContaining(String departmentName);
 
 }
