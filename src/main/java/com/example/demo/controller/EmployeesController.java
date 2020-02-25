126f28b2abde2ce973c2b01b1306813677322eba
Theerawat Phuna <theerawat@orcsoft.co.th>
2020-02-25 17:47:00 +0700

    Merge remote-tracking branch 'origin/master' into config_query
    # Conflicts:
    #	src/main/java/com/example/demo/model/Employee.java
    #	src/main/java/com/example/demo/model/JobHistory.java
    #	src/main/java/com/example/demo/repo/EmployeeRepository.java
    #	src/main/java/com/example/demo/repo/JobHistoryRepository.java
    #	src/main/java/com/example/demo/service/EmployeeService.java
    #	src/main/resources/application.properties

---
+++ src/main/java/com/example/demo/controller/EmployeesController.java
@@ -0,0 +1,32 @@
+//package com.example.demo.controller;
+//import java.util.Collection;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.PageRequest;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.domain.Sort;
+//import org.springframework.data.querydsl.binding.QuerydslPredicate;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.example.demo.model.Country;
+//import com.example.demo.repo.CountryRepository;
+//public class EmployeesController {
+	
+//	@Autowired
+//	private CountryRepository mService;
+//	
+//	@Autowired
+//	private CountryRepository mCountryRepo;
+//	
+//	
+//	@GetMapping("/")
+//	public String greeting() {
+//		return "Hello";
+//	}
+//	
+//
+//}
+
+/////////////////////////////////////////////////////////
